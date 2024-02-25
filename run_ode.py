fe6422cf553e1cb9b71b2d5bdf47bf1411d155ad
Michael Morris <49106997+M-Morris-95@users.noreply.github.com>
2024-02-25 19:26:56 +0000

    Update run_ode.py

--- run_ode.py
+++ run_ode.py
@@ -75,7 +75,7 @@ region_info = {
     },
     'US': {
         'n_regions': 1,
-        'latent_dim': 12,
+        'latent_dim': 8,
         'n_qs':90,
         'ode_params':{'net_sizes': [64, 64, 32],  'aug_net_sizes': [64, 64], 'prior_std' : 0.05},
         'dec_params':{},
@@ -87,89 +87,88 @@ region_info = {
 ode_names = ['CONN', 'UONN', 'CONNb', 'UONNb']
 test_seasons = [2015,2016,2017,2018]
 regions = ['US', 'hhs', 'state']
-regions = ['hhs', 'US']
 epoch_ls = [220,240,260, 280,300,350,400]
 nums = [15,16,17,18,19]
 uncertainty=True
 
 started_file_path = "started.txt"
 for epochs in epoch_ls:
-    for num in nums:
-        for region in regions:
-            for test_season in test_seasons:
-                for ode_name in ode_names:
-                    # setup files
-                    ode = {'CONN':Fp, 'UONN':FaFp, 'SONN':Fa, 'CONNb':Bayes_Fp, 'UONNb':Bayes_FaFp, 'SONNb':Bayes_Fa}[ode_name]
+    for latent_dim in [8,10,6]:
+        for num in nums:
+            for region in regions:
+                for test_season in test_seasons:
+                    for ode_name in ode_names:
+                        # setup files
+                        ode = {'CONN':Fp, 'UONN':FaFp, 'SONN':Fa, 'CONNb':Bayes_Fp, 'UONNb':Bayes_FaFp, 'SONNb':Bayes_Fa}[ode_name]
 
-                    common_prefix = f'{region}/{ode_name}/{test_season}_{epochs}_{num}_'
+                        common_prefix = f'{region}/{ode_name}/{test_season}_{epochs}_{latent_dim}_{num}_'
 
-                    file_prefix = f'weights/{common_prefix}'
-                    norm_prefix = f'norms/{common_prefix}'
-                    chkpt_prefix = f'chkpts/{common_prefix}'
+                        file_prefix = f'weights/{common_prefix}'
+                        norm_prefix = f'norms/{common_prefix}'
+                        chkpt_prefix = f'chkpts/{common_prefix}'
 
-                    # check if already running
-                    lock_path = started_file_path + ".lock"
-                    with FileLock(lock_path):
-                        run=True
-                        with open(started_file_path, 'r') as file:
-                            content = file.read().splitlines()
-                        
-                        for c in content:
-                            a=1
-                            if file_prefix in c:
-                                run = False
-                        if run:
-                            with open(started_file_path, 'a') as file:
-                                file.write(file_prefix + '\n')
-                                    
-                    if run:      
-                        try:      
-                            print(region, ode_name, test_season, epochs, num)
-
-                            # make folders
-                            utils.make_file(chkpt_prefix)
-                            utils.make_file(file_prefix)
-                            utils.make_file(norm_prefix)
-                        
-                            # setup stuff for models
-                            losses = training_info[ode_name]
-                            n_regions = region_info[region]['n_regions']
-                            n_qs = region_info[region]['n_qs']
-                            latent_dim = region_info[region]['latent_dim']
-                            enc_params = region_info[region]['enc_params']
-                            dec_params = region_info[region]['dec_params']
-                            ode_params = region_info[region]['ode_params']
-
-                            _data = DataConstructor(test_season=test_season, region = region, window_size=window_size, n_queries=n_qs, gamma=gamma)
-                            x_train, y_train, x_test, y_test, scaler = _data(run_backward=True, no_qs_in_output=True)
-                            train_loader, x_test, y_test = convert_to_torch(x_train, y_train, x_test, y_test, batch_size=32, shuffle=True, dtype=dtype)  
-
+                        # check if already running
+                        lock_path = started_file_path + ".lock"
+                        with FileLock(lock_path):
+                            run=True
+                            with open(started_file_path, 'r') as file:
+                                content = file.read().splitlines()
                             
+                            for c in content:
+                                a=1
+                                if file_prefix in c:
+                                    run = False
+                            if run:
+                                with open(started_file_path, 'a') as file:
+                                    file.write(file_prefix + '\n')
+                                        
+                        if run:      
+                            try:      
+                                print(region, ode_name, test_season, epochs, num)
 
-                            model = VAE(Encoder_Back_GRU, ode, Decoder, n_qs, latent_dim, n_regions, file_prefix=file_prefix, chkpt_prefix=chkpt_prefix, ode_params=ode_params, enc_params=enc_params, dec_params=dec_params, uncertainty=uncertainty, ode_kl_w = 1/153)
-                            model.setup_training(lr=lr)
+                                # make folders
+                                utils.make_file(chkpt_prefix)
+                                utils.make_file(file_prefix)
+                                utils.make_file(norm_prefix)
+                            
+                                # setup stuff for models
+                                losses = training_info[ode_name]
+                                n_regions = region_info[region]['n_regions']
+                                n_qs = region_info[region]['n_qs']
+                                enc_params = region_info[region]['enc_params']
+                                dec_params = region_info[region]['dec_params']
+                                ode_params = region_info[region]['ode_params']
 
-                            for i in range(2,5):
-                                eval_pts = [0,7,14,21,28][:i]
-                                time_steps = t[:(eval_pts[-1]+1)]
+                                _data = DataConstructor(test_season=test_season, region = region, window_size=window_size, n_queries=n_qs, gamma=gamma)
+                                x_train, y_train, x_test, y_test, scaler = _data(run_backward=True, no_qs_in_output=True)
+                                train_loader, x_test, y_test = convert_to_torch(x_train, y_train, x_test, y_test, batch_size=32, shuffle=True, dtype=dtype)  
+
                                 
-                                model.train(train_loader, 
-                                            time_steps, 
-                                            int(epochs/4), 
-                                            losses, 
-                                            eval_pts, 
-                                            n_samples = n_samples, 
-                                            grad_lim=5000, 
-                                            checkpoint=True, 
-                                            track_norms=True, 
-                                            norm_file=f'{norm_prefix}norms.txt', 
-                                            disable=True, 
-                                            validate = {'x_test':x_test, 'y_test':y_test, 't':t, 'scaler':scaler, 'n_samples':32})
-                                                    
-                            model.save()
-                            utils.append_to_line(started_file_path, file_prefix, append = 'finished')
-                            utils.test(model, scaler, x_test, y_test, t, test_season=test_season, variables = {'epochs':epochs, 'ode_name':ode_name, 'region':region, 'latent_dim':latent_dim, 'num':num}, n_samples = 128, file_name='results_table.csv')
-                        except:
-                            utils.append_to_line(started_file_path, file_prefix, append = 'failed')
-                        
-                
+
+                                model = VAE(Encoder_Back_GRU, ode, Decoder, n_qs, latent_dim, n_regions, file_prefix=file_prefix, chkpt_prefix=chkpt_prefix, ode_params=ode_params, enc_params=enc_params, dec_params=dec_params, uncertainty=uncertainty, ode_kl_w = 1/153)
+                                model.setup_training(lr=lr)
+
+                                for i in range(2,5):
+                                    eval_pts = [0,7,14,21,28][:i]
+                                    time_steps = t[:(eval_pts[-1]+1)]
+                                    
+                                    model.train(train_loader, 
+                                                time_steps, 
+                                                int(epochs/4), 
+                                                losses, 
+                                                eval_pts, 
+                                                n_samples = n_samples, 
+                                                grad_lim=5000, 
+                                                checkpoint=True, 
+                                                track_norms=True, 
+                                                norm_file=f'{norm_prefix}norms.txt', 
+                                                disable=True, 
+                                                validate = {'x_test':x_test, 'y_test':y_test, 't':t, 'scaler':scaler, 'n_samples':32})
+                                                        
+                                model.save()
+                                utils.append_to_line(started_file_path, file_prefix, append = 'finished')
+                                utils.test(model, scaler, x_test, y_test, t, test_season=test_season, variables = {'epochs':epochs, 'ode_name':ode_name, 'region':region, 'latent_dim':latent_dim, 'num':num}, n_samples = 128, file_name='results_table.csv')
+                            except:
+                                utils.append_to_line(started_file_path, file_prefix, append = 'failed')
+                            
+                    
